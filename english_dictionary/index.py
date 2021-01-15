b78cf5359134b3df3c2202efe54dc724993db557
Nav Dhamu <n4vdeep@gmail.com>
2021-01-15 21:43:32 +0000

    added word likeness feature

--- english_dictionary/index.py
+++ english_dictionary/index.py
@@ -1,14 +1,26 @@
 import json
 import difflib
 
-from difflib import  SequenceMatcher
+from difflib import  SequenceMatcher, get_close_matches
+
+data = json.load(open("data.json"))
+# print(data.keys())
+
+message = "Unrecognized Word"
 
 def meaning_of(word):
-    data = json.load(open("data.json"))
-    message = "Unrecognized Word"
-    try:
+    if word in data:
         return data[word]
-    except KeyError:
+    elif len(get_close_matches(word, data.keys())) > 0:
+        confirmation_msg = input(f"Did you mean '{get_close_matches(word, data.keys())[0]}'? Enter Y if Yes, N if No: ")
+        if confirmation_msg == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif confirmation_msg == "N":
+            pass
+        else:
+            print("Sorry that response is not recognised, Please respond with Y (Yes) or N (No)")
+            confirmation_msg = input(f"Did you mean '{get_close_matches(word, data.keys())[0]}'? Enter Y if Yes, N if No: ")
+    else:
         return f"{message}"
 
 word = input("Enter a word: ").lower()
