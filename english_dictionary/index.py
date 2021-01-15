88da7359c649796d6bfa97e3286033ec10ca833e
Nav Dhamu <n4vdeep@gmail.com>
2021-01-15 22:23:44 +0000

    added conditionals for different case scenarios and better displaying of output

--- english_dictionary/index.py
+++ english_dictionary/index.py
@@ -9,8 +9,13 @@ data = json.load(open("data.json"))
 message = "Unrecognized Word"
 
 def meaning_of(word):
+    word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[word.title()]
+    elif word.upper() in data: #if user entered "usa" this will check for "USA" as well.
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         confirmation_msg = input(f"Did you mean '{get_close_matches(word, data.keys())[0]}'? Enter Y if Yes, N if No: ")
         if confirmation_msg == "Y":
@@ -22,6 +27,12 @@ def meaning_of(word):
     else:
         return f"{message}"
 
-word = input("Enter a word: ").lower()
+word = input("Enter a word: ")
 
-print(meaning_of(word))
+output = meaning_of(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
